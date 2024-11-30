b3496a5d4e07f9126c294598a315ba81c949e75b
Frostyl <lgrobelny66@gmail.com>
2024-12-01 00:12:40 +0100

    Update UML diagram

--- src/main/java/pl/put/poznan/transformer/logic/InverseTextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/InverseTextTransformer.java
@@ -15,11 +15,11 @@ public class InverseTextTransformer extends TransformerDecorator{
         return adjustCase(text, newText.toString());
     }
 
-    private String adjustCase(String originalText, String inversedText) {
+    private String adjustCase(String originalText, String invertedText) {
         StringBuilder adjustedText = new StringBuilder();
         for (int i = 0; i < originalText.length(); i++) {
-            if(Character.isUpperCase(originalText.charAt(i))) adjustedText.append(Character.toUpperCase(inversedText.charAt(i)));
-            if(Character.isLowerCase(originalText.charAt(i))) adjustedText.append(Character.toLowerCase(inversedText.charAt(i)));
+            if(Character.isUpperCase(originalText.charAt(i))) adjustedText.append(Character.toUpperCase(invertedText.charAt(i)));
+            if(Character.isLowerCase(originalText.charAt(i))) adjustedText.append(Character.toLowerCase(invertedText.charAt(i)));
         }
         return adjustedText.toString();
     }
