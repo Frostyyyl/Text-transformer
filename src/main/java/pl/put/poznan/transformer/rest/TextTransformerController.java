e2763f0ae10b0796d28efb9d35ee7170fd30d0d3
Frostyl <lgrobelny66@gmail.com>
2024-12-07 19:56:24 +0100

    Updated the logger DEBUG and INFO messages

--- src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
@@ -19,10 +19,14 @@ public class TextTransformerController {
                         @RequestParam(value = "transforms", defaultValue = "upper, escape") String[] transforms) {
 
         // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
+        logger.debug("The 'GET' method input text: {}.", text);
+        logger.debug("The 'GET' method input transforms: {}", Arrays.toString(transforms));
 
         String transformedText = performTransformation(text, transforms);
+
+        // log the result
+        logger.debug("The 'GET' method output text: {}", transformedText);
+
         return new Response(transformedText);
     }
 
@@ -31,10 +35,14 @@ public class TextTransformerController {
                          @RequestBody String[] transforms) {
 
         // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
+        logger.debug("The 'POST' method input text: {}.", text);
+        logger.debug("The 'POST' method input transforms: {}", Arrays.toString(transforms));
 
         String transformedText = performTransformation(text, transforms);
+
+        // log the result
+        logger.debug("The 'POST' method output text: {}", transformedText);
+
         return new Response(transformedText);
     }
 
@@ -47,34 +55,42 @@ public class TextTransformerController {
                 case "upper":
                     UpperTextTransformer upperTextTransformer = new UpperTextTransformer(textTransformer);
                     result = upperTextTransformer.transform(result);
+                    logger.info("Performed the 'upper' transformation.");
                     break;
                 case "lower":
                     LowerTextTransformer lowerTextTransformer = new LowerTextTransformer(textTransformer);
                     result = lowerTextTransformer.transform(result);
+                    logger.info("Performed the 'lower' transformation.");
                     break;
                 case "capitalize":
                     CapitalizeTextTransformer capitalizeTextTransformer = new CapitalizeTextTransformer(textTransformer);
                     result = capitalizeTextTransformer.transform(result);
+                    logger.info("Performed the 'capitalize' transformation.");
                     break;
                 case "inverse":
                     InverseTextTransformer inverseTextTransformer = new InverseTextTransformer(textTransformer);
                     result = inverseTextTransformer.transform(result);
+                    logger.info("Performed the 'inverse' transformation.");
                     break;
                 case "numbers_to_text":
                     NumberToTextTransformer numberToTextTransformer = new NumberToTextTransformer(textTransformer);
                     result = numberToTextTransformer.transform(result);
+                    logger.info("Performed the 'numbers_to_text' transformation.");
                     break;
                 case "expand_shortcuts":
                     ExpandShortcutsTextTransformer expandShortcutsTextTransformer = new ExpandShortcutsTextTransformer(textTransformer);
                     result = expandShortcutsTextTransformer.transform(result);
+                    logger.info("Performed the 'expand_shortcuts' transformation.");
                     break;
                 case "abbreviate":
                     AbbreviateTextTransformer abbreviateTextTransformer = new AbbreviateTextTransformer(textTransformer);
                     result = abbreviateTextTransformer.transform(result);
+                    logger.info("Performed the 'abbreviate' transformation.");
                     break;
                 case "remove_duplicates":
                     RemoveDuplicatesTextTransformer removeDuplicatesTextTransformer = new RemoveDuplicatesTextTransformer(textTransformer);
                     result = removeDuplicatesTextTransformer.transform(result);
+                    logger.info("Performed the 'remove_duplicates' transformation.");
                     break;
                 default:
                     break;
