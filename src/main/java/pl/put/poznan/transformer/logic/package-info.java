aca6a0438b3e72f054ae413ba3750b15582b1629
Frostyl <lgrobelny66@gmail.com>
2025-01-17 21:55:12 +0100

    Add package info

---
+++ src/main/java/pl/put/poznan/transformer/logic/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains classes related to text transformation.
+ * It defines the core {@link pl.put.poznan.transformer.logic.Transformer} interface and provides multiple
+ * implementations and decorators to modify the behavior of text transformation.
+ */
+package pl.put.poznan.transformer.logic;
